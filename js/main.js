1c56a2c240d5bb0ff0073f1b1d0cee5d8bccd4de
chungit201 <vuongvanchung30092001@gmail.com>
2022-08-11 11:43:08 +0700

    new update

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function openPopup(i) {
+    $('.cover').show();
+    $('.popup').hide();
+    $('#popup-send-code').show();
+    gtag('event', 'show', { 'event_category': 'registration' });
+    if (i === 1) {
+        gtag('event', 'click', {
+            'event_category': 'registration',
+            'event_label': 'button_whitelist_registration_1'
+        });
+    }
+    if (i === 2) {
+        gtag('event', 'click', {
+            'event_category': 'registration',
+            'event_label': 'button_whitelist_registration_2'
+        });
+    }
+}
+
+function closePopup() {
+    $('.cover').hide();
+    $('.popup').hide();
+}
+
+function prePopup() {
+    $('#popup-confirm-code').hide();
+    $('#popup-send-code').show();
+}
